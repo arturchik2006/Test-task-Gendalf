8e430e8dbb593e7b002b6d06ff29208e3ad3a29e
arturik193 <atknnwkk249@gmail.com>
2025-04-24 17:42:34 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,11 @@ npm run build
 
 # navigate into the build output directory
 cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:<arturchik2006>/<Test-task-Gendalf>.git main:gh-pages
+
+cd -
